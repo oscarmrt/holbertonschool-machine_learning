1d3bfa77037cca7bc472052ca8b8be29b81f19ad
oscarmrt <oscarmrt@hotmail.com>
2020-07-17 04:21:15 +0000

    file 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Program that calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """Function that calculates the shape of a numpy.ndarray"""
+    return matrix.shape
