8acf51a5f5c13bb005f3020dc4e178c335208a50
oscarmrt <oscarmrt@hotmail.com>
2020-07-17 03:25:07 +0000

    file 6

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Program that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """Function that concatenates two arrays"""
+    concArray = []
+    concArray = arr1 + arr2
+    return concArray
