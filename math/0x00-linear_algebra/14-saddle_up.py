f1f11a54bbff2b998281aaf38ff0b747785e6d0a
oscarmrt <oscarmrt@hotmail.com>
2020-07-17 04:42:07 +0000

    file 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Program that performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Function that performs matrix multiplication"""
+    return np.dot(mat1, mat2)
