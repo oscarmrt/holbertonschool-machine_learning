efed53c18410aeb78455e00a7aac7c94273bbb8e
oscarmrt <oscarmrt@hotmail.com>
2020-07-30 21:02:44 +0000

    modified file 17

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,5 +4,16 @@
 
 def poly_integral(poly, C=0):
     """Function that calculates the integral of a polynomial"""
-    if type(poly) is not list or len(poly) == 0 or type(C) is None:
+    if type(poly) is not list or len(poly) == 0 or type(C) is not int:
         return None
+    elif poly == [0]:
+        return [C]
+    else:
+        integralPoly = [C]
+        for x in range(len(poly)):
+            y = poly[x] / (x + 1)
+            if y.is_integer():
+                integralPoly.append(int(y))
+            else:
+                integralPoly.append(y)
+        return integralPoly
