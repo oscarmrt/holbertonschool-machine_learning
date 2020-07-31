f3a33d6110ff5595e6e55871189f7ca126f9b9b3
oscarmrt <oscarmrt@hotmail.com>
2020-07-31 04:59:55 +0000

    modified file 10

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -9,13 +9,14 @@ class Binomial():
 
     def __init__(self, data=None, n=1, p=0.5):
         """Class constructor"""
+        self.n = int(n)
+        self.p = float(p)
         if data is None:
-            if n <= 0:
+            if self.n <= 0:
                 raise ValueError('n must be a positive value')
-            if p < 0 or p > 1:
+            if self.p <= 0 or self.p >= 1:
                 raise ValueError('p must be greater than 0 and less than 1')
-            self.n = int(n)
-            self.p = float(p)
+
         else:
             if type(data) is not list:
                 raise TypeError('data must be a list')
