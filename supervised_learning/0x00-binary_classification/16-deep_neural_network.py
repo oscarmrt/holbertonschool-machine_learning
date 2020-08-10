5e74fb07f37363bbede144e290b9572dd0870857
oscarmrt <oscarmrt@hotmail.com>
2020-08-09 21:05:04 -0500

    modified type error for layers

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -13,7 +13,7 @@ class DeepNeuralNetwork():
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) is not list or !layers:
+        if type(layers) is not list or not layers:
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
