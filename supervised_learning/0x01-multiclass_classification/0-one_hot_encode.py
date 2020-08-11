58301a541ed8f9177f7325ffaf57c74297dfc048
oscarmrt <oscarmrt@hotmail.com>
2020-08-11 10:06:42 -0500

    file 0

---
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Program that converts a numeric label vector into a one-hot matrix"""
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """Function that converts a numeric label vector into a one-hot matrix"""
+    if len(Y) == 0 or type(classes) is not int:
+        return None
+    onehot_encoder = np.zeros((classes, len(Y)))
+    onehot_encoder[Y, np.arange(len(Y))] = 1
+    return onehot_encoder
