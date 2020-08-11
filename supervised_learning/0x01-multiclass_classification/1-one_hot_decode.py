3e165d6fb9c94566814e95ad326a69f579d8a80f
oscarmrt <oscarmrt@hotmail.com>
2020-08-11 10:50:09 -0500

    file 1

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -7,9 +7,8 @@ def one_hot_decode(one_hot):
     """Function that converts a one-hot matrix into a vector of labels"""
     if len(one_hot) == 0:
         return None
-    elif len(one_hot.shape) != 2:
-        return None
     elif not isinstance(one_hot, np.ndarray):
         return None
-    else:
-        return np.argmax(one_hot, axis=0)
+    elif len(one_hot.shape) != 2:
+        return None
+    return np.argmax(one_hot, axis=0)
