e21c8bb116da82c344b843bd3314a5186dae114a
oscarmrt <oscarmrt@hotmail.com>
2020-08-16 12:19:42 -0500

    new directory, README.md file and file 0

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""""This program returns two placeholders, x and y, for the neural network"""
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    """Funtion that returns two placeholders,
+    x and y, for the neural network"""
+    x = tf.placeholder('float', shape=[None, nx], name='x')
+    y = tf.placeholder('float', shape=[None, classes], name='y')
