c7fee5d4d8a8c669d84c3db31f399e19ab9cf110
oscarmrt <oscarmrt@hotmail.com>
2020-08-16 19:35:56 -0500

    modified file 0

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -8,3 +8,4 @@ def create_placeholders(nx, classes):
     x and y, for the neural network"""
     x = tf.placeholder('float', shape=[None, nx], name='x')
     y = tf.placeholder('float', shape=[None, classes], name='y')
+    return x, y
