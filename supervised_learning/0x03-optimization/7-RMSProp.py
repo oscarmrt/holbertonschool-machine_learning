cef27eec1ba7530ae143735a0bb2d8b5548f0199
oscarmrt <oscarmrt@hotmail.com>
2020-08-23 20:45:11 -0500

    file 7

---
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python3
+"""Program that updates a variable
+using the RMSProp optimization algorithm"""
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """Program that updates a variable using the
+    RMSProp optimization algorithm"""
+    sdv = beta2 * s + (1 - beta2) * (grad ** 2)
+    var = var - alpha * (grad / (np.sqrt(sdv) + epsilon))
+    return var, sdv
