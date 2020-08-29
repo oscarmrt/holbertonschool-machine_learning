151ffbecc52d1fccf29541f9474365e9cfa80b51
oscarmrt <oscarmrt@hotmail.com>
2020-08-28 20:21:25 -0500

    modified file 5

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -8,15 +8,19 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """Function that updates the weights of a neural network
     with Dropout regularization using gradient descent."""
     m = Y.shape[1]
-    dz = cache['A'+str(L)] - Y
+    dAl = cache['A' + str(L)] - Y
     for i in reversed(range(1, L + 1)):
-        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        dW = ((1 / m) * np.matmul(cache['A'+str(i-1)], dz.T))
+        W_key = 'W' + str(i)
+        b_key = 'b' + str(i)
+        A_key = 'A' + str(i - 1)
+        D_key = 'D' + str(i - 1)
+        dw = (np.matmul(cache[A_key], dAl.T) / m)
+        db = (np.sum(dAl, axis=1, keepdims=True) / m)
         if i - 1 > 0:
-            dz = np.matmul(weights['W'+str(i)].T, dz) *\
-                ((1 - cache['A'+str(i-1)] ** 2)) *\
-                (cache['D'+str(i-1)] / keep_prob)
-            weights['W'+str(i)] = weights['W'+str(i)] -\
-                (alpha * dW).T
-            weights['b'+str(i)] = weights['b'+str(i)] -\
-                (alpha * db)
+            dAl = np.matmul(weights['W' + str(
+                i)].T, dAl) * (1 - (cache[A_key] ** 2)) * (cache[D_key]
+                                                           / keep_prob)
+        weights[W_key] = weights['W' + str(
+            i)] - (alpha * dw).T
+        weights[b_key] = weights['b' + str(
+            i)] - (alpha * db)
