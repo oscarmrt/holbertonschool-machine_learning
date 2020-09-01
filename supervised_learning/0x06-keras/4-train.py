7988627e29e5d130b1ee9dd5c82f022fc185c541
oscarmrt <oscarmrt@hotmail.com>
2020-09-01 17:14:05 -0500

    file 4

---
+++ supervised_learning/0x06-keras/4-train.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Program that trains a model using mini-batch gradient descent"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """Function that trains a model using mini-batch gradient descent"""
+    history = network.fit(x=data, y=labels, batch_size=batch_size,
+                          epochs=epochs, verbose=verbose, shuffle=shuffle)
+    return history
