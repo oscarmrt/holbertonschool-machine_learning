763fdc75e527e4959040c1bf062cbffea821fb66
oscarmrt <oscarmrt@hotmail.com>
2020-09-01 18:36:06 -0500

    modified file 6

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Program that trains a model using mini-batch gradient descent"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """Function that trains a model using mini-batch gradient descent"""
+    if early_stopping and validation_data:
+        callback = [K.callbacks.EarlyStopping(monitor='val_loss',
+                                              patience=patience)]
+        history = network.fit(x=data, y=labels, batch_size=batch_size,
+                              epochs=epochs, verbose=verbose, shuffle=shuffle,
+                              validation_data=validation_data,
+                              callbacks=callback)
+    else:
+        history = network.fit(x=data, y=labels, batch_size=batch_size,
+                              epochs=epochs, verbose=verbose, shuffle=shuffle,
+                              validation_data=None)
+    return history
