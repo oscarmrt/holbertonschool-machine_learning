a5ffff91902f32e4f410f5e5e05df5f3712c590d
oscarmrt <oscarmrt@hotmail.com>
2020-10-26 21:45:02 -0500

    modified multinormal.py

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -18,3 +18,18 @@ class MultiNormal:
         deviation = np.tile(self.mean.reshape(-1), n).reshape(n, d)
         cov = data.T - deviation
         self.cov = np.matmul(cov.T, cov)/(n - 1)
+
+    def pdf(self, x):
+        """public instance method that calculates the PDF at a data point"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.mean.shape[0]
+        if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        inv = np.linalg.inv(self.cov)
+        det = np.linalg.det(self.cov)
+        a = 1 / np.sqrt((((2 * np.pi)**(x.shape[0]) * det)))
+        inv = np.matmul((x - self.mean).T, inv)
+        b = np.exp(-(1/2) * ((np.matmul(inv, (x - self.mean)))))
+        pdf = a * b
+        return pdf[0][0]
