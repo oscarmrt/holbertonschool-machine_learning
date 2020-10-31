69636724a3329ff6976250e74a9ee038217611d8
oscarmrt <oscarmrt@hotmail.com>
2020-10-30 21:03:34 -0500

    modified file 3

---
+++ math/0x07-bayesian_prob/3-posterior.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+"""Program that calculates the posterior probability for the various
+hypothetical probabilities of developing severe side effects
+given the data"""
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """Function that calculates the likelihood of obtaining
+    this data given various hypothetical probabilities of
+    developing severe side effects"""
+    if not isinstance(n, int) or n < 1:
+        raise ValueError("n must be a positive integer")
+    if type(x) != int or x < 0:
+        err = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(err)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if np.amin(P) < 0 or np.amax(P) > 1:
+        raise ValueError("All values in P must be in the range [0, 1]")
+    fact = np.math.factorial
+    tmp = fact(n) / (fact(x) * fact(n - x))
+    lh = tmp * (P**x) * ((1 - P)**(n - x))
+    return lh
+
+
+def intersection(x, n, P, Pr):
+    """Function that that calculates the intersection of obtaining
+    this data with the various hypothetical probabilities"""
+    if not isinstance(n, int) or n < 1:
+        raise ValueError("n must be a positive integer")
+    if type(x) != int or x < 0:
+        err = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(err)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.amin(P) < 0 or np.amax(P) > 1:
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.amin(Pr) < 0 or np.amax(Pr) > 1:
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+    lh = likelihood(x, n, P)
+    Intr = lh * Pr
+    return Intr
+
+
+def marginal(x, n, P, Pr):
+    """Function that calculates the marginal probability
+    of obtaining the data"""
+    if not isinstance(n, int) or n < 1:
+        raise ValueError("n must be a positive integer")
+    if type(x) != int or x < 0:
+        err = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(err)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.amin(P) < 0 or np.amax(P) > 1:
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.amin(Pr) < 0 or np.amax(Pr) > 1:
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+    marg = np.sum(intersection(x, n, P, Pr))
+    return marg
+
+
+def posterior(x, n, P, Pr):
+    """Function that calculates the posterior probability for the various
+    hypothetical probabilities of developing severe side
+    effects given the data"""
+    if not isinstance(n, int) or n < 1:
+        raise ValueError("n must be a positive integer")
+    if type(x) != int or x < 0:
+        err = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(err)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.amin(P) < 0 or np.amax(P) > 1:
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.amin(Pr) < 0 or np.amax(Pr) > 1:
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+    Intr = intersection(x, n, P, Pr)
+    marg = marginal(x, n, P, Pr)
+    postr = Intr / marg
+    return postr
