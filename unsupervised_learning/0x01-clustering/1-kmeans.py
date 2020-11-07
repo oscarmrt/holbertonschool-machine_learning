0d4b2bc39f47a6f2e27c2f8fc1ed5301a42c3892
oscarmrt <oscarmrt@hotmail.com>
2020-11-06 20:53:39 -0500

    modified file 1 conditions

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -6,9 +6,11 @@ import numpy as np
 def kmeans(X, k, iterations=1000):
     """Function that performs K-means on a dataset"""
     if type(X) is not np.ndarray or len(X.shape) != 2:
-        return None
-    if type(k) is not int or k < 1:
-        return None
+        return None, None
+    if type(k) is not int or k <= 0:
+        return None, None
+    if type(iterations) is not int or iterations <= 0:
+        return None, None
     n, d = X.shape
     min = np.amin(X, axis=0)
     max = np.amax(X, axis=0)
