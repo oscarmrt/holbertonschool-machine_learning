cc000f588f402415b6f6b33a5b020800fecd510f
oscarmrt <oscarmrt@hotmail.com>
2020-11-30 15:53:04 +0000

    file 0

---
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Program that creates an autoencoder"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """Function that creates an autoencoder"""
+    inputs = keras.Input(shape=(input_dims,))
+    encoded = keras.layers.Dense(hidden_layers[0], activation='relu')(inputs)
+    for i in range(1, len(hidden_layers)):
+                encoded = keras.layers.Dense(hidden_layers[i],
+                                             activation='relu')(encoded)
+    encoded = keras.layers.Dense(latent_dims, activation='relu')(encoded)
+    encoder = keras.Model(inputs, encoded)
+    latentInputs = keras.Input(shape=(latent_dims,))
+    decoded = latentInputs
+    for i in range(len(hidden_layers) - 1, -1, -1):
+        decoded = keras.layers.Dense(hidden_layers[i],
+                                     activation='relu')(decoded)
+    decoded = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
+    decoder = keras.Model(latentInputs, decoded)
+    auto = keras.Model(inputs, decoder(encoder(inputs)))
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+    return encoder, decoder, auto
