4498252a784c05cf01d2e757156bc554207ab0c3
oscarmrt <oscarmrt@hotmail.com>
2020-11-30 16:27:09 +0000

    fixed pycodestyle for file 0

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -9,7 +9,7 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     encoded = keras.layers.Dense(hidden_layers[0], activation='relu')(inputs)
     for i in range(1, len(hidden_layers)):
         encoded = keras.layers.Dense(hidden_layers[i],
-                                             activation='relu')(encoded)
+                                     activation='relu')(encoded)
     encoded = keras.layers.Dense(latent_dims, activation='relu')(encoded)
     encoder = keras.Model(inputs, encoded)
     latentInputs = keras.Input(shape=(latent_dims,))
