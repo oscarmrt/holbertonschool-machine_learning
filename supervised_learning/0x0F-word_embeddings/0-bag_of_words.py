18e082d596965824e0b409edbb9300fb22824501
oscarmrt <oscarmrt@hotmail.com>
2020-12-18 23:43:43 +0000

    modified file 0

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -5,4 +5,6 @@
 def bag_of_words(sentences, vocab=None):
     """Function that creates a bag of words embedding
     matrix"""
-    
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    return (X.toarray(), vectorizer.get_feature_names())
