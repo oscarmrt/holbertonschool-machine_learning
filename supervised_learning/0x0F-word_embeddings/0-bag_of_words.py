c69dccc3e6518d5b92a9a6b1c96f44e081d4042d
oscarmrt <oscarmrt@hotmail.com>
2020-12-18 23:46:08 +0000

    modified file 0 import

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Program that creates a bag of words embedding matrix"""
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
