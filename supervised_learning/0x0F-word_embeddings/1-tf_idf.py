809f05ec338ff40be716334391fa74a880b0abb9
oscarmrt <oscarmrt@hotmail.com>
2020-12-19 00:14:50 +0000

    modified file 1

--- supervised_learning/0x0F-word_embeddings/1-tf_idf.py
+++ supervised_learning/0x0F-word_embeddings/1-tf_idf.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """Program that creates a TF-IDF embedding"""
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
     """Function that creates a TF-IDF embedding"""
-    
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    return (X.toarray(), vectorizer.get_feature_names())
