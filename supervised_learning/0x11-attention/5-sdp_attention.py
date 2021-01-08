3bb58991d835c0471fcaafb82f6c75ad8dbb1883
oscarmrt <oscarmrt@hotmail.com>
2021-01-08 19:49:33 +0000

    file 5

---
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Program that calculates the scaled dot product attention"""
+import tensorflow as tf
+
+
+def sdp_attention(Q, K, V, mask=None):
+    """Function that calculates the scaled dot product attention"""
+    matmul_qk = tf.matmul(Q, K, transpose_b=True)
+    dk = tf.cast(tf.shape(K)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+    output = tf.matmul(attention_weights, V)
+    return output, attention_weights
