17e914308a64f92b344b6a8b952fff7fe76947b8
oscarmrt <oscarmrt@hotmail.com>
2021-01-08 23:13:26 +0000

    modified file 9

---
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Class Encoder"""
+import tensorflow as tf
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+
+
+class Encoder(tf.keras.layers.Layer):
+    """class Encoder"""
+    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
+                 drop_rate=0.1):
+        """Class constructor"""
+        super(Encoder, self).__init__()
+        self.N = N
+        self.dm = dm
+        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for i in range(N)]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    call(self, x, training, mask):
+    """Public instance method that returns a tensor of shape
+    (batch, input_seq_len, dm) containing the encoder output"""
+    seq_len = tf.shape(x)[1]
+    x = self.embedding(x)
+    x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+    x += tf.cast(self.positional_encoding, dtype=tf.float32)[:seq_len]
+    x = self.dropout(x, training=training)
+    for i in range(self.N):
+        x = self.blocks[i](x, training, mask)
+    return x
