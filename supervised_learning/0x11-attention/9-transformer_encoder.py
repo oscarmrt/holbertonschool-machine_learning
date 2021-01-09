a3e66ba757b8ee5fabdcb7e03f1994306ec7da8d
oscarmrt <oscarmrt@hotmail.com>
2021-01-09 00:58:36 +0000

    modified file 9

--- supervised_learning/0x11-attention/9-transformer_encoder.py
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -15,17 +15,16 @@ class Encoder(tf.keras.layers.Layer):
         self.dm = dm
         self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for i in range(N)]
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask):
     """Public instance method that returns a tensor of shape
     (batch, input_seq_len, dm) containing the encoder output"""
-    seq_len = tf.shape(x)[1]
-    position = tf.cast(self.positional_encoding, dtype=tf.float32)
+    seq_len = x.shape[1]
     x = self.embedding(x)
     x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
-    x += position[:seq_len]
+    x += self.positional_encoding[:seq_len]
     x = self.dropout(x, training=training)
     for i in range(self.N):
         x = self.blocks[i](x, training, mask)
