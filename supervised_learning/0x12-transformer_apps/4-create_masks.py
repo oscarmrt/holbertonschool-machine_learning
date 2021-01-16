4ebdb760657e1684b02df653804302c4c8229bcc
oscarmrt <oscarmrt@hotmail.com>
2021-01-16 02:24:37 +0000

    modified file 4

--- supervised_learning/0x12-transformer_apps/4-create_masks.py
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -9,6 +9,7 @@ def create_masks(inputs, target):
     encoder_mask = encoder_mask[:, tf.newaxis, tf.newaxis, :]
     decoder_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
     decoder_mask = decoder_mask[:, tf.newaxis, tf.newaxis, :]
+    size = tf.shape(target)[1]
     look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
     dec_target_mask = tf.cast(tf.math.equal(target, 0), tf.float32)
     dec_target_mask = dec_target_mask[:, tf.newaxis, tf.newaxis, :]
