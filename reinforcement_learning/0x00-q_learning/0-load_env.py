d1f0fe024832b24d2deff85b0bea3e203aee10a9
oscarmrt <oscarmrt@hotmail.com>
2021-02-09 16:48:14 +0000

    file 0

---
+++ reinforcement_learning/0x00-q_learning/0-load_env.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Program that loads the pre-made FrozenLakeEnv evnironment
+from OpenAI’s gym"""
+import gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """Function that loads the pre-made FrozenLakeEnv evnironment
+from OpenAI’s gym"""
+    env = gym.make("FrozenLake-v0", desc=desc, map_name=map_name,
+                   is_slippery=is_slippery)
+    return env
