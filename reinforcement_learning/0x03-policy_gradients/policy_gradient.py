c060026fb6b6cc50ce36f4ecc9920d62312618a3
oscarmrt <oscarmrt@hotmail.com>
2021-03-12 13:22:34 -0500

    modified file policy_gradient.py with task 1

--- reinforcement_learning/0x03-policy_gradients/policy_gradient.py
+++ reinforcement_learning/0x03-policy_gradients/policy_gradient.py
@@ -8,3 +8,16 @@ def policy(matrix, weight):
     z = matrix.dot(weight)
     exp = np.exp(z)
     return exp / exp.sum()
+
+
+def policy_gradient(state, weight):
+    """Function that computes the Monte-Carlo policy
+    gradient based on a state and a weight matrix"""
+    P = policy(state, weight)
+    action = np.random.choice(len(P[0]), p=P[0])
+    s = P.reshape(-1, 1)
+    softmax = np.diagflat(s) - np.dot(s, s.T)
+    softmax = softmax[action, :]
+    dlog = softmax / P[0, action]
+    gradient = state.T.dot(dlog[None, :])
+    return action, gradient
