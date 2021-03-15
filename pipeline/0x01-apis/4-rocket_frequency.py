7c9569e619f0b13138cb523dd60bc7ddde76bf2e
oscarmrt <oscarmrt@hotmail.com>
2021-03-15 16:30:43 -0500

    mfile 4

---
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Program that By using the (unofficial) SpaceX API, write
+a script that displays the number of launches per rocket"""
+import requests
+import sys
+import time
+
+if __name__ == '__main__':
+    url = "https://api.spacexdata.com/v4/launches"
+    r = requests.get(url)
+    launches = {}
+    for i in r.json():
+        if i['rocket'] not in launches:
+            launches[i['rocket']] = 1
+        else:
+            launches[i['rocket']] += 1
+    url = "https://api.spacexdata.com/v4/rockets/"
+    r = requests.get(url)
+    rockets = []
+    for i in r.json():
+        if i['id'] in launches:
+            rockets.append({'rocket': i['name'],
+                            'launches': launches[i['id']]})
+        else:
+            continue
+    launches = sorted(rockets, key=lambda i: i['rocket'])
+    launches = sorted(rockets, key=lambda i: i['launches'], reverse=True)
+    for i in launches:
+        print("{}: {}".format(i['rocket'], i['launches']))
