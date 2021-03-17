90316ecf2093b5d93a042411b7149ef41e0578ed
oscarmrt <oscarmrt@hotmail.com>
2021-03-17 00:52:02 -0500

    file 3

--- pipeline/0x02-databases/0-create_database_if_missing.sql
+++ pipeline/0x02-databases/0-create_database_if_missing.sql
@@ -1,2 +1,3 @@
 -- creates the database db_0 in your MySQL server.
+
 CREATE DATABASE IF NOT EXISTS db_0;
--- pipeline/0x02-databases/2-list_values.sql
+++ pipeline/0x02-databases/2-list_values.sql
@@ -1,2 +1,3 @@
 -- lists all rows of the table first_table in your MySQL server.
+
 SELECT * FROM first_table;
