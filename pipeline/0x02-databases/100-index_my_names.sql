e8d30bdeb1dcad4eb0ef87f2375e3e991afcd748
oscarmrt <oscarmrt@hotmail.com>
2021-03-17 23:56:57 -0500

    file 100

---
+++ pipeline/0x02-databases/100-index_my_names.sql
@@ -0,0 +1,7 @@
+-- Write a SQL script that creates an index idx_name_first on the table names and the first letter of name.
+-- Requirements:
+-- Import this table dump: names.sql.zip
+-- Only the first letter of name must be indexed
+-- Context: Index is not the solution for any performance issue, but well used, it’s really powerful!
+
+CREATE INDEX idx_name_first ON names (name(1));
